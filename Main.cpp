c0221543bc3227b5c1cbfe61f8468af8944434b4
giranath <giranath@gmail.com>
2013-04-05 12:52:17 -0400

    Plus d'accent et compatibilité entre Windows et Mac
    J'ai configuré le projet pour qu'il soit compatible entre nos 2
    machines et aussi j'ai retirer les accents des commentaires

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,7 @@ Auteur: Maxime Fontaine Bombardier
 Date: 03/04/2013
 Description du programme: Space invader en SDL
 
-Directives au pré-processeur
+Directives au pre-processeur
 =================================*/
 
 #include "SDL_Utils.h"
@@ -23,8 +23,8 @@ using namespace std;
 
 const int	LARGEUR_ALIEN=50,         // La largeur en pixel d'un alien
 			HAUTEUR_ALIEN=50,         // La hauteur en pixel d'un alien
-			LARGEUR_HERO=50,          // La largeur en pixel du héro
-			HAUTEUR_HERO=50,          // La hauteur en pixel du héro
+			LARGEUR_HERO=50,          // La largeur en pixel du hero
+			HAUTEUR_HERO=50,          // La hauteur en pixel du hero
 			LARGEUR_BLOC=10,          // Le nombre de colone d'alien dans le bloc
 			HAUTEUR_BLOC=5;           // Le nombre de ligne d'alien dans le bloc
 
@@ -34,63 +34,63 @@ int main(int argc, char *argv[])
 {
 	
 	/*************** Variable SDL *********************/
-	// Les différentes surfaces du programme
-	SDL_Surface *ecran = 0;            // L'écran
-	SDL_Surface *spriteHero = 0;       // Le sprite du héro
+	// Les differentes surfaces du programme
+	SDL_Surface *ecran = 0;            // L'ecran
+	SDL_Surface *spriteHero = 0;       // Le sprite du hero
 	SDL_Surface *spriteAlien = 0;      // Le sprite d'un alien
-	SDL_Surface *spriteBalleHero = 0;  // Le sprite d'une balle du héro
+	SDL_Surface *spriteBalleHero = 0;  // Le sprite d'une balle du hero
 	SDL_Surface *spriteBalleAlien = 0; // Le sprite d'une balle alien
 
 	
     // Les rectangles
-	SDL_Rect posHero = {200,450},      // Vecteur de la position du héro
+	SDL_Rect posHero = {200,450},      // Vecteur de la position du hero
 			 posAlien,                 // Vecteur de la position du bloc d'alien
-			 velAlien,                 // La vélocité du bloc d'alien
-			 velHero,                  // La vélocité du héro
-			 posBalleHero;             // La position de la balle unique du héro
+			 velAlien,                 // La velocite du bloc d'alien
+			 velHero,                  // La velocite du hero
+			 posBalleHero;             // La position de la balle unique du hero
 
 	vector<SDL_Rect> posBalleAlien;    // Un tableau dynamique des balles aliens
 
 	/*************** Variable Standards *********************/
-	bool	partieTerminee = false,    // La partie est terminée
-			droitTirer = true,         // Le héro à le droit de tirer
-			toucheD=0,                 // La touche Droite est appuyée
-			toucheG=0,                 // La touche Gauche est appuyée
-			toucheH=0;                 // La touche Haut ou même espace est appuyée
+	bool	partieTerminee = false,    // La partie est terminee
+			droitTirer = true,         // Le hero a le droit de tirer
+			toucheD=0,                 // La touche Droite est appuyee
+			toucheG=0,                 // La touche Gauche est appuyee
+			toucheH=0;                 // La touche Haut ou même espace est appuyee
 
-	unsigned int	score = 0;         // Le score de la présente partie
+	unsigned int	score = 0;         // Le score de la presente partie
 
 	int		vie = 3;                   // Le nombre de vie du joueur
 
-	unsigned long ticks;               // Le nombre de milisecondes écoulées depuis le lancement du programme
+	unsigned long ticks;               // Le nombre de milisecondes ecoulees depuis le lancement du programme
 
 	int Alien[HAUTEUR_BLOC][LARGEUR_BLOC]; // Matrice contenant les informations du bloc d'aliens
 
 	/*************** PROGRAMME *********************/
 	// Initialisation SDL
-	if(SDL_Init(SDL_INIT_VIDEO) == -1) // Si elle échoue…
+	if(SDL_Init(SDL_INIT_VIDEO) == -1) // Si elle echoue…
 	{
 		cout << "Impossible d initialiser SDL !"; // On averti l'utilisateur et on quitte
 		return EXIT_FAILURE;
 	}
 	
-	// On charge l'image du héro à l'aide de la bibliothèque SDL_Images
+	// On charge l'image du hero a l'aide de la bibliotheque SDL_Images
 	spriteHero = IMG_Load("SpaceInvaders.bmp");
 
-    // On crée l'écran à l'aide de la fonction initEcran
+    // On cree l'ecran a l'aide de la fonction initEcran
 	ecran = initEcran("Space Invaders", NULL, 400, 600, 0, 0, 0);
 	
 	/*************** BOUCLE PRINCIPALE *********************/
-	while(partieTerminee == false)         // Tant que la partie n'est pas terminée...
+	while(partieTerminee == false)         // Tant que la partie n'est pas terminee...
 	{
-		SDL_Event event;                  // On récupère l'évènement de la fenêtre sans bloquer le programme
+		SDL_Event event;                  // On recupere l'evenement de la fenêtre sans bloquer le programme
 		SDL_PollEvent(&event);            
 		switch(event.type)
 		{
 		case SDL_QUIT:                    // Si on appui sur le bouton de fermeture de la fenêtre...
 			partieTerminee = true;         // On quitte la boucle
 			break;
-			case SDL_KEYDOWN:/* Si appui d'une touche, on sauvegarde son état dans le booleen correspondant*/
+			case SDL_KEYDOWN:/* Si appui d'une touche, on sauvegarde son etat dans le booleen correspondant*/
 					switch (event.key.keysym.sym)
 					{
 					case SDLK_UP: case SDLK_SPACE:
@@ -102,9 +102,11 @@ int main(int argc, char *argv[])
 						case SDLK_RIGHT:
 							toucheD = 1;
 							break;
+                        default:
+                            break;
 					}
 					break;
-				case SDL_KEYUP:/* Si retire d'une touche, on sauvegarde son état dans le booleen correspondant*/
+				case SDL_KEYUP:/* Si retire d'une touche, on sauvegarde son etat dans le booleen correspondant*/
 					switch (event.key.keysym.sym)
 					{
 					case SDLK_UP: case SDLK_SPACE:
@@ -116,27 +118,29 @@ int main(int argc, char *argv[])
 						case SDLK_RIGHT:
 							toucheD = 0;
 							break;
+                        default:
+                            break;
 					}
 					break;
 		}
 		
-		// On réinitialise la velocité du héro à 0
+		// On reinitialise la velocite du hero a 0
 		velHero.x = 0;
 
         // Si on appui sur la touche de droite...		
 		if(toucheD)
-			velHero.x = 1;   // La vélocité du héro est 1 sur son axe x
+			velHero.x = 1;   // La velocite du hero est 1 sur son axe x
 		if(toucheG)
 			velHero.x = -1; 
 			
-		// On déplace le héro en fonction de sa vélocité
+		// On deplace le hero en fonction de sa velocite
 		posHero.x += velHero.x;
 		
 
-        // On remplit l'écran de noir
+        // On remplit l'ecran de noir
 		SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 0, 0, 0));
 		
-		// On déssine le héro
+		// On dessine le hero
 		SDL_BlitSurface(spriteHero, NULL, ecran, &posHero);
 		
 		// On change les buffers
@@ -147,7 +151,7 @@ int main(int argc, char *argv[])
 
 	}
 	
-	// On libère la mémoire de la surface
+	// On libere la memoire de la surface
 	SDL_FreeSurface(spriteHero);
 	
 	// On ferme la SDL
--- SDL_Utils.h
+++ SDL_Utils.h
@@ -1,31 +1,29 @@
 #ifndef DEF_SDL_UTILS
 #define DEF_SDL_UTILS
 
-#include <SDL.h>
-#include <SDL_image.h>
+#ifdef __APPLE__
+    #include <SDL/SDL.h>
+    #include <SDL_image/SDL_image.h>
+#else 
+    #include <SDL.h>
+    #include <SDL_image.h>
+#endif
+
 #include <iostream>
-// Implémentation =======================================================================================================
+// Implementation =======================================================================================================
 
-//void initSDL()
-//{
-//	if(SDL_Init(SDL_INIT_VIDEO) == -1)	//evenement
-//	{
-//		cout << "Erreur lors de l'initialisation de la SDL", SDL_GetError();
-//		exit(EXIT_FAILURE);
-//	}
-//}
 
-//Chargement de l'écran principal
+//Chargement de l'ecran principal
 SDL_Surface* initEcran(char titre[], char icone[], int largeur, int hauteur, int rouge, int vert, int bleu)
 {
 	SDL_Surface *ecran;
 	SDL_WM_SetCaption(titre,NULL); //titre de la fenetre
-	//Chargement de l'icône AVANT SDL_SetVideoMode, copier le bmp ds le même dossier que .ccp
+	//Chargement de l'icone AVANT SDL_SetVideoMode, copier le bmp dans le meme dossier que .cpp
 	
 	if(icone != NULL)
 		SDL_WM_SetIcon(SDL_LoadBMP(icone), NULL);
 
-	ecran = SDL_SetVideoMode(largeur,hauteur,32,SDL_HWSURFACE | SDL_RESIZABLE| SDL_DOUBLEBUF);  //la surface de écran
+	ecran = SDL_SetVideoMode(largeur,hauteur,32,SDL_HWSURFACE | SDL_RESIZABLE| SDL_DOUBLEBUF);  //la surface de l'ecran
 	SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, rouge, vert, bleu));
 	return ecran;
 }
@@ -53,12 +51,12 @@ SDL_Rect initPosition(int x, int y)
 	return pos;
 }
 
-//Fonction remplissant l'écran 
+//Fonction remplissant l'ecran 
 void dessinerImage(SDL_Surface* image, SDL_Rect posImage, SDL_Surface* ecran, SDL_Rect posBonhomme)
 {
 	SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 255, 255, 255));		//l'endroit et quel couleur
 	SDL_BlitSurface(image, &posImage, ecran, &posBonhomme);	//Remplir la surface
-	SDL_Flip(ecran);						//mettre à jour l'écran avec les nouvelles données
+	SDL_Flip(ecran);						//mettre a jour l'ecran avec les nouvelles donnees
 }
 
 #endif
