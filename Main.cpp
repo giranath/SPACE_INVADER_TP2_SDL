50e4c85ed30d8502d930a58521f4d629def8da20
giranath <giranath@gmail.com>
2013-04-05 14:03:05 -0400

    Ajout d'un fonction de création d'entité
    Une fonction creerEntite permet d'automatiser le processus de création
    d'une entité!

--- Main.cpp
+++ Main.cpp
@@ -16,19 +16,23 @@ using namespace std;
 /*Structures
  ================================*/
 
+// Ici c'est uniquement une sorte de camouflage. Donc il s'agit d'un SDL_Rect mais on peut aussi l'appelé Vecteur2f
 typedef SDL_Rect Vecteur2f;
 
 // Structure pour définir un objet dans le jeu
 struct Entite {
-    bool vivant;            // L'entite est-il vivant?
-    int vie;                // Son nombre de vie
+    bool vivant = true;                 // L'entite est-il vivant?
+    int vie = 1;                        // Son nombre de vie
     
     Vecteur2f position = {0,0,0,0};     // Sa position dans l'espace
     Vecteur2f velocite = {0,0,0,0};     // Sa vélocité sur 2 dimensions
-    SDL_Rect  hitbox;       // Boite de collision
-    SDL_Surface *surface = 0;   // Surface SDL
+    float largeur;                      // Sa largeur en pixel
+    float hauteur;                      // Sa hauteur en pixel
+    
+    SDL_Surface *surface = 0;           // Surface SDL
 };
 
+Entite creerEntite(int vie, Vecteur2f position, float largeur, float hauteur);
 
 /* Programme principale
 =================================*/
@@ -43,30 +47,26 @@ const float	VITESSE_BALLE = 1;          // La vitesse d'une balle
 
 int main(int argc, char *argv[])
 {
-	
 	/*************** Variable SDL *********************/
 	// Les differentes surfaces du programme
-	SDL_Surface *ecran = 0;            // L'ecran
+	SDL_Surface *ecran = 0;             // L'ecran
 
-	Entite joueur;
-    Entite balleHero;
+	Entite joueur;                      // Il s'agit du hero (le vaisseau vert)
+    Entite balleHero;                   // Il s'agit du projectile du héro (1 seul à la fois)
     
-    // Les rectangles
 	Vecteur2f posAlien,                 // Vecteur de la position du bloc d'alien
-              velAlien;                // La velocite du bloc d'alien
+              velAlien;                 // La velocite du bloc d'alien
 
-	vector<Entite> balleAlien;    // Un tableau dynamique des balles aliens
+	vector<Entite> balleAlien;          // Un tableau dynamique des balles aliens
 
 	/*************** Variable Standards *********************/
 	bool	partieTerminee = false,    // La partie est terminee
 			droitTirer = true,         // Le hero a le droit de tirer
-			toucheD=0,                 // La touche Droite est appuyee
-			toucheG=0,                 // La touche Gauche est appuyee
-			toucheH=0;                 // La touche Haut ou même espace est appuyee
+			toucheD = false,                 // La touche Droite est appuyee
+			toucheG = false,                 // La touche Gauche est appuyee
+			toucheH = false;                 // La touche Haut ou même espace est appuyee
 
 	unsigned int	score = 0;         // Le score de la presente partie
-
-	int		vie = 3;                   // Le nombre de vie du joueur
 
 	unsigned long ticks;               // Le nombre de milisecondes ecoulees depuis le lancement du programme
 
@@ -79,16 +79,15 @@ int main(int argc, char *argv[])
 		cout << "Impossible d initialiser SDL !"; // On averti l'utilisateur et on quitte
 		return EXIT_FAILURE;
 	}
-	
-	// On charge l'image du hero a l'aide de la bibliotheque SDL_Images
+    
+    joueur = creerEntite(3, {200,500}, 10, 10);
+    
+    // On charge l'image du hero a l'aide de la bibliotheque SDL_Images
 	joueur.surface = SDL_LoadBMP("SpaceInvaders.bmp");
     
     if(!joueur.surface) {
         cerr << "Impossible de charger l'image" << endl;
     }
-    
-    joueur.position.x = 200;
-    joueur.position.y = 500;
     
     // On cree l'ecran a l'aide de la fonction initEcran
 	ecran = initEcran((char*)"Space Invaders", NULL, 400, 600, 0, 0, 0);
@@ -96,6 +95,7 @@ int main(int argc, char *argv[])
 	/*************** BOUCLE PRINCIPALE *********************/
 	while(partieTerminee == false)         // Tant que la partie n'est pas terminee...
 	{
+        ticks = SDL_GetTicks();            // On récupère les ticks de l'application
 		SDL_Event event;                  // On recupere l'evenement de la fenêtre sans bloquer le programme
 		SDL_PollEvent(&event);            
 		switch(event.type)
@@ -107,13 +107,13 @@ int main(int argc, char *argv[])
 					switch (event.key.keysym.sym)
 					{
 					case SDLK_UP: case SDLK_SPACE:
-							toucheH = 1;
+							toucheH = true;
 							break;
 						case SDLK_LEFT:
-							toucheG = 1;
+							toucheG = true;
 							break;
 						case SDLK_RIGHT:
-							toucheD = 1;
+							toucheD = true;
 							break;
                         default:
                             break;
@@ -123,13 +123,13 @@ int main(int argc, char *argv[])
 					switch (event.key.keysym.sym)
 					{
 					case SDLK_UP: case SDLK_SPACE:
-							toucheH = 0;
+							toucheH = false;
 							break;
 						case SDLK_LEFT:
-							toucheG = 0;
+							toucheG = false;
 							break;
 						case SDLK_RIGHT:
-							toucheD = 0;
+							toucheD = false;
 							break;
                         default:
                             break;
@@ -173,4 +173,21 @@ int main(int argc, char *argv[])
 	
 	// On quitte
  	return EXIT_SUCCESS;
+}
+
+// Fonction automatisant la creation d'entite.  Parametres: Le nombre de vie, la position initialie, la largeur et la hauteur
+//                                              Retour: Un entite dont les attributs correspondent aux parametres
+Entite creerEntite(int vie, Vecteur2f position, float largeur, float hauteur) {
+    Entite entite;
+    
+    entite.vivant = true;                   // Par défaut, l'entite est vivant
+    entite.vie = vie;                       // On lui definit son nombre de vie
+    entite.position = position;             // On lui assigne une position initiale
+    entite.largeur = largeur;               // On lui donne la largeur souhaite
+    entite.hauteur = hauteur;               // On lui donne la hauteur souhaite
+    
+    entite.velocite.x = 0;                  // À sa création, l'entité est immobile
+    entite.velocite.y = 0;
+    
+    return entite;                          // On retourne le nouvel entité
 }
